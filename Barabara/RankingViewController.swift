120198de45a41e62fb9224f0fcad35b7a35943b4
Rinnosuke Saeki <macrin1559@gmail.com>
2022-05-13 02:24:41 +0900

    rank

---
+++ Barabara/RankingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RankingViewController.swift
+//  Barabara
+//
+//  Created by 佐伯凜乃介 on 2022/05/12.
+//
+
+import UIKit
+
+class RankingViewController: UIViewController {
+    
+    @IBOutlet var rankingLabel1: UILabel!
+    @IBOutlet var rankingLabel2: UILabel!
+    @IBOutlet var rankingLabel3: UILabel!
+
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        rankingLabel1.text = String(defaults.integer(forKey: "score1"))
+        rankingLabel2.text = String(defaults.integer(forKey: "score2"))
+        rankingLabel3.text = String(defaults.integer(forKey: "score3"))
+
+    }
+                                    
+    @IBAction func toTop() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
